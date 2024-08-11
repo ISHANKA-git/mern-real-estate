5e57f593a82e89d42297a0b59890b8de89048566
Ishanka Rusith <87255708+ISHANKA-git@users.noreply.github.com>
2024-08-11 16:16:26 +0530

    complete signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -19,15 +19,15 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return errorHandler(401, "User not found!");
+    if (!validUser) return next(errorHandler(404, 'User not found!'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return errorHandler(401, "Invalid password!");
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const { password: hashedPassword, ...userWithoutPassword } = validUser._doc;
+    const { password: pass, ...rest } = validUser._doc;
     res
-      .cookie("access_token", token, { httpOnly: true })
-      .json(userWithoutPassword)
-      .status(200);
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
