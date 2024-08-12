f0731b9492b09a9f089754f360d4e874d19ac264
Ishanka Rusith <87255708+ISHANKA-git@users.noreply.github.com>
2024-08-12 17:39:03 +0530

    Update the header and make the profile page private

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,5 +1,4 @@
-import { createSlice} from '@reduxjs/toolkit'
-
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     currentUser: null,
@@ -16,15 +15,16 @@ const userSlice = createSlice({
         },
         signInSuccess: (state, action) => {
             state.currentUser = action.payload;
-            state.error = null;
             state.loading = false;
+            state.error = null;
         },
         signInFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
-        },
-    },
+        }
+    }
 });
 
 export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+
 export default userSlice.reducer;
