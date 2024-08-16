905dd6d23f8694fc237fa86296c374f4c9ca7dba
Ishanka Rusith <87255708+ISHANKA-git@users.noreply.github.com>
2024-08-16 13:41:41 +0530

    Complete About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+    <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Home Horizon</h1>
+    <p className='mb-4 text-slate-700'>
+        Home Horizon is your trusted partner in the real estate industry, focused on providing seamless experiences for buying, selling, and renting properties in the most sought-after locations. Our seasoned agents are committed to delivering top-notch service and ensuring that each transaction is as effortless and enjoyable as possible.
+    </p>
+    <p className='mb-4 text-slate-700'>
+        Our vision is to empower our clients to achieve their real estate aspirations by offering expert guidance, personalized support, and a comprehensive understanding of the local property market. Whether you are seeking to buy, sell, or rent a home, our team is here to assist you at every turn.
+    </p>
+    <p className='mb-4 text-slate-700'>
+        With a rich background in the real estate sector, our agents bring a wealth of knowledge and expertise to the table. We are dedicated to providing exceptional service and making the process of buying or selling a property a fulfilling and exciting journey for all our clients.
+    </p>
+</div>
   )
 }
